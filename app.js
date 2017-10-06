883aad1ae6e2fe60f6faa0089425250069158054
Anoop Pallippattuillom <anooppnarayan@gmail.com>
2017-10-06 22:23:05 +0530

    app.js

--- app.js
+++ app.js
@@ -3,19 +3,17 @@ var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
 var stringify = require('node-stringify');
-
+const sql = require('mssql');
 //DB config
-var sql = require('node-sqlserver');
-
-var conn_str = "Server=tcp:sewsqlsrv01.database.windows.net,1433;"+
-				"Initial Catalog=sewsql01;"+
-				"Security Info=False;"+
-				"User ID=seqsqladmin;"+
-				"Password=Pa$$w0rd098;"+
-				"MultipleActiveResultSets=False;"+
-				"Encrypt=True;"+
-				"TrustServerCertificate=False;"+
-				"Connection Timeout=30";
+const dbconfig = {
+    user: 'seqsqladmin',
+    password: 'Pa$$w0rd098',
+    server: 'sewsqlsrv01.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+    database: 'sewsql01',
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
+}
 
 
 //REST end points
@@ -39,41 +37,19 @@ app.get('/js/:jsFile', function(req,res){
 });
 
 app.get('/getsewloc', function(req,res){
-	var sewLocQry = "select distinct sdl.[Lattitude], sdl.[Longitude], sct.[street_name] from dbo.sew_device_loc sdl, [dbo].[CRM_Data] crm where sdl.deviceData=crm.property_id";
-		    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    sql.open(conn_str, function (err, conn) {
-        if (err) {
-            res.end("Error opening the connection!");
-            return;
-        }
-        conn.queryRaw(sewLocQry, function (err, results) {
-            if (err) {
-                res.write("Error running query!");
-                return;
-            }
-            console.log("row count = " + results.rows.length + "\n");
-            res.send(results);
-        }); 
-    });
+		sql.connect(dbconfig)
+		.then(function() {
+		 const request = new sql.Request()
+				request.execute('dbo.SP_GET_SEW_LOC', (err, result) => {
+					console.log(result.recordset); // 
+					res.send(result.recordset);
+					sql.close();
+				});
+		})
+		.catch(function(err) {
+		  console.log(err);
+		});
 });
-
-var EventHubClient = require('azure-event-hubs').Client;
- 
-var eventHubClient = EventHubClient.fromConnectionString('Endpoint=sb://sewehd001.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=T8RE+I9K0/ch7xBuZpCXpFCkbrb5Pdcm7llU7usicmM=', 'sew-meter-data')
-eventHubClient.open()
-    .then(function() {
-        return eventHubClient.createReceiver('sew-leak-detection', '10', { startAfterTime: Date.now() })
-    })
-    .then(function (rx) {
-        rx.on('errorReceived', function (err) { console.log(err); }); 
-        rx.on('message', function (message) {
-            var body = message.body;
-            // See http://azure.github.io/amqpnetlite/articles/azure_sb_eventhubs.html for details on message annotation properties from EH. 
-           // var enqueuedTime = Date.parse(message.systemProperties['x-opt-enqueued-time']);
-			console.log(body);
-			socket.emit('newMsg',body);
-        });
-    });
 
 var port = normalizePort(process.env.PORT || '3000');
 http.listen(port, function() {
